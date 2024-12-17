31347d1204138872bf0603e75a9c5b0d44b82516
Priyanshu Baraiya <priyanshuahir01@gmail.com>
2024-12-17 23:30:49 +0530

    Add controller method to retrieve a single product by ID

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -43,3 +43,13 @@ export const getAllProducts = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// Get a single product
+export const getProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
